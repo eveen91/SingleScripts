9e996bf65f1899ca7901242e3bd600a1f8218d26
sjedynak <sjedynak@VCGDY102061L>
2019-02-25 18:00:07 +0100

    poprawka rename

--- SingleScripts/renameHost.ps1
+++ SingleScripts/renameHost.ps1
@@ -3,10 +3,14 @@
 #
 #$hostname = (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name
 #$hostname = $hostname + "-lap"
+$rename= { 
+	Rename-Computer -NewName "$hostnameNew" -Force 
+	Restart-Computer 
+}
 $hostname= Read-Host "Podaj nazwê now¹ hosta"
 $hostnameNew= Read-Host "Podaj nazwê now¹ hosta"
 $uname= Read-Host "podaj login:"
 $pass= Read-Host "podaj has³o:"
 $secstr = ConvertTo-SecureString $pass -AsPlainText -Force
 $newcred = New-Object System.Management.Automation.PSCredential ($uname,$secstr)
-Invoke-Command -ComputerName $hostname -ScriptBlock { Rename-Computer -NewName "$hostnameNew" -Force | Restart-Computer } -credential $newcred
+Invoke-Command -ComputerName $hostname -ScriptBlock $rename -credential $newcred
