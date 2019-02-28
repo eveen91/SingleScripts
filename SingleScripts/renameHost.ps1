f92eaf87fae20dfc9b0223979c91eefb20865483
sjedynak <sjedynak@VCGDY102061L>
2019-02-28 13:43:31 +0100

    using remote hostname input

--- SingleScripts/renameHost.ps1
+++ SingleScripts/renameHost.ps1
@@ -1,16 +1,9 @@
 #
 # renameHost.ps1
-#
-#$hostname = (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name
-#$hostname = $hostname + "-lap"
-$rename= { 
-	Rename-Computer -NewName "$hostnameNew" -Force 
-	Restart-Computer 
+$hostname= Read-Host "Podaj nazwê hosta"
+
+$commands={
+	$hostnameNew= Read-Host "Podaj nazwê now¹ hosta"
+	Rename-Computer -NewName "$hostnameNew" -Force -Restart -DomainCredential (Get-Credential -Credential $uname)
 }
-$hostname= Read-Host "Podaj nazwê now¹ hosta"
-$hostnameNew= Read-Host "Podaj nazwê now¹ hosta"
-$uname= Read-Host "podaj login:"
-$pass= Read-Host "podaj has³o:"
-$secstr = ConvertTo-SecureString $pass -AsPlainText -Force
-$newcred = New-Object System.Management.Automation.PSCredential ($uname,$secstr)
-Invoke-Command -ComputerName $hostname -ScriptBlock $rename -credential $newcred
+Invoke-Command -ComputerName $hostname -ScriptBlock $commands
