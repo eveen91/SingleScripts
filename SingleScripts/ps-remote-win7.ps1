77e96a7fb1540171da379e6745e3a2c2e44ec8dd
sjedynak <sjedynak@VCGDY102061L>
2019-03-06 10:38:29 +0100

    ps win 7 update

--- SingleScripts/ps-remote-win7.ps1
+++ SingleScripts/ps-remote-win7.ps1
@@ -3,10 +3,5 @@
 #
 #Skip network location setting for pre-Vista operating systems 
 
-if([environment]::OSVersion.version.Major -lt 6) { return } 
-if(1,3,4,5 -contains (Get-WmiObject win32_computersystem).DomainRole) { return } 
-$networkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}")) 
-$connections = $networkListManager.GetNetworkConnections() 
-$connections | % {$_.GetNetwork().SetCategory(1)}
-Set-Item WSMan:\localhost\Client\TrustedHosts *
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
 Enable-PSRemoting -Force
