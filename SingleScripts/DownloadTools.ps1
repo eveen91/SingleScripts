f2d0459cc63acc5659114558e3da50fa2ae559c2
sjedynak <sjedynak@VCGDY102061L>
2019-03-18 14:07:37 +0100

    downloading all tools

--- SingleScripts/DownloadTools.ps1
+++ SingleScripts/DownloadTools.ps1
@@ -2,18 +2,12 @@
 # PsTools.ps1
 #
 $destination= 'C:\Users\vectrauser\AppData\Local\DwiTools\'
-function get_dropbox_file($file)
-{
-	$token=Z5-BtjmM42AAAAAAAAAAD_w63qwP6Nc-0y8mJj-36iyO6qd8n6OwwjwL1NRApBK2
-	$uri=https://content.dropboxapi.com/2/files/download
-	$header=""
-	Invoke-WebRequest -Uri -Headers
-}
 
 if( -Not (Test-Path -Path $destination ) )
 {
    New-Item -ItemType directory -Path $destination
 }
-
-#Invoke-RestMethod
-#curl -X POST https://content.dropboxapi.com/2/files/download \ --header "Authorization: Bearer <Z5-BtjmM42AAAAAAAAAAD_w63qwP6Nc-0y8mJj-36iyO6qd8n6OwwjwL1NRApBK2>" \ --header "Dropbox-API-Arg: {\"path\": \"/Homework/math/Prime_Numbers.txt\"}" \ -o "./Prime_Numbers.txt"
+Invoke-WebRequest -Uri https://www.dropbox.com/sh/q0ebixtjz0vf62a/AADf3C4EM-0tK5rZjohWmc1Va?dl=1 -OutFile $destination"tools.zip"
+#Invoke-WebRequest -Uri https://www.dropbox.com/s/riheysgjtiz1eag/PsModulesList.txt?dl=1 -OutFile $destination"PsModulesList.txt"
+Expand-Archive C:\Users\vectrauser\AppData\Local\DwiTools\tools.zip -DestinationPath C:\Users\vectrauser\AppData\Local\DwiTools\
+Remove-Item C:\Users\vectrauser\AppData\Local\DwiTools\tools.zip
--- SingleScripts/PsTools.ps1
+++ SingleScripts/PsTools.ps1
@@ -3,14 +3,14 @@
 #
 
 #main
-$select=-1
+$select=99
 while($select -ne 0 )
 {
-	Write-Host"lista funkcji\n"
-	Write-Host"1. Raport b³êdów\n"
-	Write-Host"2. Wykonaj skrypt\n"
-	Write-Host"3. Instalacja modu³ów powershell\n"
-	Write-Host"4. Windows Update\n"
-	Write-Host"5. checklista zmian\n"
-	Read-Host=$select
+	Write-Host "lista funkcji"
+	Write-Host "1. Raport b³êdów"
+	Write-Host "2. Wykonaj skrypt"
+	Write-Host "3. Instalacja modu³ów powershell"
+	Write-Host "4. Windows Update"
+	Write-Host "5. checklista zmian"
+	$select=Read-Host
 }
