24d59fd40c9d298d394995aee4f4bc13a4bfd2af
sjedynak <sjedynak@VCGDY102061L>
2019-03-18 10:57:28 +0100

    module list, download from dropbox

--- SingleScripts/PsTools.ps1
+++ SingleScripts/PsTools.ps1
@@ -1,3 +1,4 @@
 #
 # PsTools.ps1
 #
+curl -X POST https://content.dropboxapi.com/2/files/download \ --header "Authorization: Bearer <Z5-BtjmM42AAAAAAAAAAD_w63qwP6Nc-0y8mJj-36iyO6qd8n6OwwjwL1NRApBK2>" \ --header "Dropbox-API-Arg: {\"path\": \"/Homework/math/Prime_Numbers.txt\"}" \ -o "./Prime_Numbers.txt"
