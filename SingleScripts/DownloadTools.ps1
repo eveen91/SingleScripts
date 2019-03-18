94d361b6c66de9a593c8a6e7bf3810363217718e
sjedynak <sjedynak@VCGDY102061L>
2019-03-18 12:39:21 +0100

    rename of script, function get_dropbox_file

--- SingleScripts/DownloadTools.ps1
+++ SingleScripts/DownloadTools.ps1
@@ -1,15 +1,20 @@
 #
 # PsTools.ps1
 #
+function get_dropbox_file($file)
+{
+	$token=Z5-BtjmM42AAAAAAAAAAD_w63qwP6Nc-0y8mJj-36iyO6qd8n6OwwjwL1NRApBK2
+	$uri=https://content.dropboxapi.com/2/files/download
+	$header
+	Invoke-WebRequest -Uri -Headers
+}
+
+
 $destination= 'C:\Users\vectrauser\AppData\Local\DwiTools\'
 
 if( -Not (Test-Path -Path $destination ) )
 {
    New-Item -ItemType directory -Path $destination
-}
-else
-{
-	
 }
 
 #Invoke-RestMethod
