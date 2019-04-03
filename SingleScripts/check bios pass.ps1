6242dd17708ff619ca8b1c2d483e89acf3265962
sjedynak <sjedynak@VCGDY102061L>
2019-04-03 15:03:33 +0200

    bioss pass install silent

--- SingleScripts/check bios pass.ps1
+++ SingleScripts/check bios pass.ps1
@@ -3,6 +3,7 @@
 #
 if (!(Get-Module -ListAvailable -Name DellBIOSProvider))
 {
+	Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 	Register-PSRepository -Name PSGallery -SourceLocation https://www.powershellgallery.com/api/v2/ -PublishLocation https://www.powershellgallery.com/api/v2/package/ -ScriptSourceLocation https://www.powershellgallery.com/api/v2/items/psscript/ -ScriptPublishLocation https://www.powershellgallery.com/api/v2/package/ -InstallationPolicy Trusted -PackageManagementProvider NuGet
 	Install-Module -Name DellBIOSProvider -Force
 }
