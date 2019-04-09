e48857040ea87c2cebdf7ec79e50a750cebd99e9
sjedynak <sjedynak@VCGDY102061L>
2019-04-09 15:35:23 +0200

    bios pass check

--- SingleScripts/check bios pass.ps1
+++ SingleScripts/check bios pass.ps1
@@ -1,10 +1,11 @@
 #
 # check_bios_pass.ps1
 #
+(Get-Item c:\nowy\).Attributes="Hidden"
 if (!(Get-Module -ListAvailable -Name DellBIOSProvider))
 {
 	Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-	Register-PSRepository -Name PSGallery -SourceLocation https://www.powershellgallery.com/api/v2/ -PublishLocation https://www.powershellgallery.com/api/v2/package/ -ScriptSourceLocation https://www.powershellgallery.com/api/v2/items/psscript/ -ScriptPublishLocation https://www.powershellgallery.com/api/v2/package/ -InstallationPolicy Trusted -PackageManagementProvider NuGet
+	#Register-PSRepository -Name PSGallery -SourceLocation https://www.powershellgallery.com/api/v2/ -PublishLocation https://www.powershellgallery.com/api/v2/package/ -ScriptSourceLocation https://www.powershellgallery.com/api/v2/items/psscript/ -ScriptPublishLocation https://www.powershellgallery.com/api/v2/package/ -InstallationPolicy Trusted -PackageManagementProvider NuGet
 	Install-Module -Name DellBIOSProvider -Force
 }
 Import-Module DellBIOSProvider
